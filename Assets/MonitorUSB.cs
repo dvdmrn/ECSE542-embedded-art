f7fd2dceab94dfae176844bee6fc75ba28e65294
dvdmrn <dgm.marino@gmail.com>
2019-10-15 20:15:42 -0400

    video switching

--- Assets/MonitorUSB.cs
+++ Assets/MonitorUSB.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 
 public class MonitorUSB : MonoBehaviour
 {
-
+// https://answers.unity.com/questions/1024305/gameobject-find-nullreferenceexception-object-refe.html lol
     public bool debug;
     public int devices;
 
     int lastAmountOfDevices;
     public int inferredDeviceCount;
+    GameObject[] FoundObject;
 
     void Awake(){
         inferredDeviceCount = 0;
@@ -18,6 +19,10 @@ public class MonitorUSB : MonoBehaviour
         lastAmountOfDevices = devices;
         print("henlo");
         StartCoroutine("PingUSB");
+        FoundObject = GameObject.FindGameObjectsWithTag("Quad");
+        foreach (GameObject obj in FoundObject) {
+            obj.SetActive (false);    
+        }
     }
 
     int getNumberOfUSB(){
@@ -44,13 +49,27 @@ public class MonitorUSB : MonoBehaviour
 
     IEnumerator PingUSB(){
         while(true){
+
             devices = getNumberOfUSB();
             if(devices > lastAmountOfDevices){ // chunking like this because sometimes a phone shows up as >1 device...
                 inferredDeviceCount++;
-                lastAmountOfDevices = devices;
+                Debug.Log("in");
+                for(int i = 0; i < FoundObject.Length; i++)  
+                {
+                    FoundObject[i].SetActive(i == inferredDeviceCount);
+                }
+            //  foreach (GameObject obj in FoundObject) {
+            //     if(obj.name == "Quad "+ inferredDeviceCount) {
+            //         obj.SetActive (true);    
+            //     }
+            //  }
+            //        FoundObject[3-inferredDeviceCount].SetActive(true);
+                     lastAmountOfDevices = devices;
             }
             else if(devices < lastAmountOfDevices){
+                //FoundObject[3-inferredDeviceCount].SetActive(false);
                 inferredDeviceCount--;
+                //FoundObject[3-inferredDeviceCount].SetActive(true);
                 lastAmountOfDevices = devices;
             }
             yield return new WaitForSeconds(1f);
