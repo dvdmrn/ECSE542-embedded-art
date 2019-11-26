b15cb3ae873fdee9f85dc752a9b20c35171f8f81
dvdmrn <dgm.marino@gmail.com>
2019-11-26 17:41:47 -0500

    new menu and particles

---
+++ Assets/Bouncey.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bouncey : MonoBehaviour
+{
+    Image img;
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        transform.position = new Vector3(pos.x, pos.y + Mathf.Sin(Mathf.Cos(Time.time)));
+        float a = Mathf.Sin(Time.time * 2f)*0.01f;
+        //print(a);
+        img.color += new Color(0f, 0f, 0f, a);
+
+    }
+}
---
+++ Assets/FancyTextMotions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FancyTextMotions : MonoBehaviour
+{
+    TextMeshProUGUI tmp;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmp = GetComponent<TextMeshProUGUI>();
+    }
+
+    float Rand()
+    {
+        return Random.Range(0f, 1.0f);
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float sin = Mathf.Sin(Time.time)*0.02f;
+        tmp.color = new Color(0.9f + (Mathf.Sin(Time.time)*0.2f), 0.9f,0.95f);
+        transform.localScale = new Vector3(transform.localScale.x + sin, 
+                                           transform.localScale.y + sin,1f);
+
+    }
+}
